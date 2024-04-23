be8474786f65322076ebae9dffaae06c6b4dca7a
Neko <lowpolyneko@protonmail.ch>
2024-04-22 19:11:14 -0500

    vectorize arraymin

--- iterative.c
+++ iterative.c
@@ -1,12 +1,26 @@
+#include <immintrin.h>
+#include <limits.h>
+
 #include"min.h"
 
 int arraymin(int* array, int size) {
-	int min=array[0];
-	for(unsigned i=1;i<size;i++) {
-		if(array[i]<min) {
-			min=array[i];
-		}
+	int min = INT_MAX;
+	int vsize = size & ~0xF;
+	unsigned i = 0;
+	
+	for (; i < vsize; i += 16) {
+		__m512i a = _mm512_load_si512(array + i);
+		int res = _mm512_reduce_min_epi32(a);
+
+		if (res < min)
+			min = res;
 	}
+
+	for (; i < size; ++i) {
+		if (array[i] < min)
+			min = array[i];
+	}
+
 	return min;
 }
 
