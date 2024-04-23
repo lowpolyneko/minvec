7dc39af9af81a451c01d70a27eb8f500ea6c0319
Neko <lowpolyneko@protonmail.ch>
2024-04-23 12:53:23 -0500

    vectorize minindex

--- iterative.c
+++ iterative.c
@@ -8,6 +8,7 @@ int arraymin(int* array, int size) {
 	int vsize = size & ~0xF;
 	unsigned i = 0;
 	
+	// batch compare 16 int blocks
 	for (; i < vsize; i += 16) {
 		__m512i a = _mm512_load_si512(array + i);
 		int res = _mm512_reduce_min_epi32(a);
@@ -16,10 +17,10 @@ int arraymin(int* array, int size) {
 			min = res;
 	}
 
-	for (; i < size; ++i) {
+	// leftovers
+	for (; i < size; ++i)
 		if (array[i] < min)
 			min = array[i];
-	}
 
 	return min;
 }
@@ -29,13 +30,37 @@ int arraymin64(int* array) {
 }
 
 int minindex(int* array, int size) {
-	int min=array[0];
-	int index=0;
-	for(unsigned i=1;i<size;i++) {
-		if(array[i]<min) {
-			min=array[i];
-			index=i;
+	int min = INT_MAX;
+	int index = -1; 
+	int vsize = size & ~0xF;
+	unsigned i = 0, min_block = 0;
+ 
+	// batch compare, keeping track of what block the minimum was found
+	for (; i < vsize; i += 16) {
+		__m512i a = _mm512_load_si512(array + i);
+		int res = _mm512_reduce_min_epi32(a);
+
+		if (res < min) {
+			min = res;
+			min_block = i;
 		}
 	}
-	return index;
+
+	// leftovers, setting the index as needed
+	for (; i < size; ++i) {
+		if (array[i] < min) {
+			min = array[i];
+			index = i;
+		}
+	}
+
+	if (index != -1)
+		return index;
+
+	// find index from block if not found in leftovers
+	for (i = min_block; i < min_block + 15; ++i)
+		if (array[i] == min)
+			return i;
+
+	return i;
 }
