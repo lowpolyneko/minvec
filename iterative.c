78e2c723be5b6cee9d9eb5d669f8e1896c396091
Neko <lowpolyneko@protonmail.ch>
2024-04-23 13:49:26 -0500

    check if minimum exists first before finding it

--- iterative.c
+++ iterative.c
@@ -4,17 +4,21 @@
 #include"min.h"
 
 int arraymin(int* array, int size) {
-	int min = INT_MAX;
 	int vsize = size & ~0xF;
 	unsigned i = 0;
+	int min = INT_MAX;
+	__m512i min_vec = _mm512_set1_epi32(min);
 	
 	// batch compare 16 int blocks
 	for (; i < vsize; i += 16) {
 		__m512i a = _mm512_load_si512(array + i);
-		int res = _mm512_reduce_min_epi32(a);
+		__mmask16 res = _mm512_cmplt_epi32_mask(a, min_vec);
 
-		if (res < min)
-			min = res;
+		if (res) {
+			// minimum exists, find it!
+			min = _mm512_reduce_min_epi32(a);
+			min_vec = _mm512_set1_epi32(min);
+		}
 	}
 
 	// leftovers
@@ -30,18 +34,21 @@ int arraymin64(int* array) {
 }
 
 int minindex(int* array, int size) {
-	int min = INT_MAX;
 	int index = -1; 
 	int vsize = size & ~0xF;
 	unsigned i = 0, min_block = 0;
+	int min = INT_MAX;
+	__m512i min_vec = _mm512_set1_epi32(min);
  
 	// batch compare, keeping track of what block the minimum was found
 	for (; i < vsize; i += 16) {
 		__m512i a = _mm512_load_si512(array + i);
-		int res = _mm512_reduce_min_epi32(a);
+		__mmask16 res = _mm512_cmplt_epi32_mask(a, min_vec);
 
-		if (res < min) {
-			min = res;
+		if (res) {
+			// minimum exists, find it!
+			min = _mm512_reduce_min_epi32(a);
+			min_vec = _mm512_set1_epi32(min);
 			min_block = i;
 		}
 	}
